382c4b911c3393b0c899260d485bd599d6cd9205
Tillo <tillo.bosshart@gmail.com>
2021-12-02 22:14:00 +0100

    pins to setting

--- booth.py
+++ booth.py
@@ -149,8 +149,8 @@ def main():
     if settings.ON_RASPI:
         GPIO.setwarnings(False) # Ignore warning for now
         GPIO.setmode(GPIO.BCM) # Use physical pin numbering
-        GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low>
-        GPIO.add_event_detect(4,GPIO.FALLING,callback=button_callback) # Setup event on pin 10 rising edge
+        GPIO.setup(settings.BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low>
+        GPIO.add_event_detect(settings.BUTTON_PIN,GPIO.FALLING,callback=button_callback) # Setup event on pin 10 rising edge
         message = input("Press enter to quit\n\n") # Run until someone presses enter
         GPIO.cleanup() # Clean up
 
